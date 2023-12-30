0b089a06aa79a5f46959f696a1c6a8560e0c2646
PackmanDude <packmandude@protonmail.com>
2023-12-30 19:45:54 +0200

    Add Component translation method implementation

--- src/main/java/GettextWrapper.java
+++ src/main/java/GettextWrapper.java
@@ -4,6 +4,7 @@ import java.text.MessageFormat;
 import java.util.Locale;
 import java.util.ResourceBundle;
 import net.kyori.adventure.key.Key;
+import net.kyori.adventure.text.Component;
 import org.jetbrains.annotations.NotNull;
 
 @lombok.ToString @lombok.EqualsAndHashCode
@@ -13,6 +14,10 @@ public final class GettextWrapper implements net.kyori.adventure.translation.Tra
 	@Override public @NotNull MessageFormat translate(final @NotNull String msgid, final @NotNull Locale locale) {
 		resources = ResourceBundle.getBundle(locale.toString());
 		return new MessageFormat(GettextResource.gettext(resources, msgid));
+	}
+	@Override public @NotNull Component translate(final @NotNull net.kyori.adventure.text.TranslatableComponent component, final @NotNull Locale locale) {
+		resources = ResourceBundle.getBundle(locale.toString());
+		return Component.text(MessageFormat.format(GettextResource.gettext(resources, component.key()), component.arguments()));
 	}
 	public @NotNull MessageFormat ngettext(final @NotNull String msgid, final @NotNull String msgid_plural, final long n, final @NotNull Locale locale) {
 		resources = ResourceBundle.getBundle(locale.toString());

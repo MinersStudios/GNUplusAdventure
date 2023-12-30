1adfd0fbde3943692acdcb4d27a64d78cb787206
PackmanDude <packmandude@protonmail.com>
2023-12-30 17:38:44 +0200

    Add code

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # GNUplusAdventure
-GNU gettext wrapper library for TranslatableComponent.
+
+GNU gettext wrapper library for net.kyori.adventure.
+
+## Usage
+
+Add GettextWrapper to GlobalTranslator by writing:
+
+```java
+GlobalTranslator.get().addSource(ua.com.minersstudios.gnuplusadventure.GettextWrapper);
+```
+
+Then use translate() (gettext()), ngettext(), npgettext(), pgettext() to get
+translations from the resource bundle named like Locale.toString()'s output.
+If none is found, key will be returned unchanged. Thus, keys are the
+English/ASCII version of the messages and remove need for a plugin developer
+to use fallback mechanism provided by the net.kyori.adventure.text package.
---
+++ src/main/java/GettextWrapper.java
@@ -0,0 +1,29 @@
+package ua.com.minersstudios.gnuplusadventure;
+import gnu.gettext.GettextResource;
+import java.text.MessageFormat;
+import java.util.Locale;
+import java.util.ResourceBundle;
+import net.kyori.adventure.key.Key;
+import org.jetbrains.annotations.NotNull;
+
+@lombok.ToString @lombok.EqualsAndHashCode
+public final class GettextWrapper implements net.kyori.adventure.translation.Translator {
+	private ResourceBundle resources;
+	@Override public @NotNull Key name() { return Key.key("gettext", "translations"); }
+	@Override public @NotNull MessageFormat translate(final @NotNull String msgid, final @NotNull Locale locale) {
+		resources = ResourceBundle.getBundle(locale.toString());
+		return new MessageFormat(GettextResource.gettext(resources, msgid));
+	}
+	public @NotNull MessageFormat ngettext(final @NotNull String msgid, final @NotNull String msgid_plural, final long n, final @NotNull Locale locale) {
+		resources = ResourceBundle.getBundle(locale.toString());
+		return new MessageFormat(GettextResource.ngettext(resources, msgid, msgid_plural, n));
+	}
+	public @NotNull MessageFormat npgettext(final @NotNull String msgctxt, final @NotNull String msgid, final @NotNull String msgid_plural, final long n, final @NotNull Locale locale) {
+		resources = ResourceBundle.getBundle(locale.toString());
+		return new MessageFormat(GettextResource.npgettext(resources, msgctxt, msgid, msgid_plural, n));
+	}
+	public @NotNull MessageFormat pgettext(final @NotNull String msgctxt, final @NotNull String msgid, final @NotNull Locale locale) {
+		resources = ResourceBundle.getBundle(locale.toString());
+		return new MessageFormat(GettextResource.pgettext(resources, msgctxt, msgid));
+	}
+}
